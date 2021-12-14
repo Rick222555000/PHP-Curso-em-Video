0322ca54c4e8ac99cbcf48899df72483075eb04f
Rick <86621338+Rick222555000@users.noreply.github.com>
2021-12-14 17:08:16 -0300

    Create vareaveis-atributos.php

---
+++ php/aula04/vareaveis-atributos.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de vareáveis</title>
+    <link rel="stylesheet" href="../../style/template.css">
+</head>
+<body>
+    <header class="cabeca">
+        <p>
+            Área de testes PHP
+        </p>
+    </header>
+    <div class="container">
+        <div class="cabecalho">
+            <p>
+                Curso de PHP para iniciantes - por Gustavo Guanabara
+            </p>
+        </div>
+        <div class="conteudo">
+            <?php
+                $n = 4;
+                $nome = "Rikelmy";
+                echo $nome;
+            ?>
+        </div>
+        <div class="detalhe">
+        </div>
+    </div>
+    <footer class="rodape">
+        Siga-em no <a href="https://github.com/Rick222555000">GitHub</a> | Rikelmy Magno de Oliveira.
+    </footer>
+</body>
+</html>
